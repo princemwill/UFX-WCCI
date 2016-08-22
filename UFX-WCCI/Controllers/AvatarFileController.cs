e243a29e8cd4cf84ae2e39b91b710e1f71f285f2
Prince <princemwilliams@gmail.com>
2016-08-22 15:43:20 -0400

    Completed user photo upload and retrieval functions for user bio and posts.

---
+++ UFX-WCCI/Controllers/AvatarFileController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UFX_WCCI.Models;
+
+namespace UFX_WCCI.Controllers
+{
+    public class AvatarFileController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: File
+        public ActionResult Index(string id)
+        {
+            var fileToRetrieve = db.Users.Find(id);
+
+            if (fileToRetrieve.PhotoBytes != null && fileToRetrieve.PhotoType != null)
+                return File(fileToRetrieve.PhotoBytes, fileToRetrieve.PhotoType);
+
+            return View();
+        }
+    }
+}
---
+++ UFX-WCCI/Controllers/PostFileController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UFX_WCCI.Models;
+
+namespace UFX_WCCI.Controllers
+{
+    public class PostFileController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: File
+        public ActionResult Index(int id)
+        {
+            var fileToRetrieve = db.Postings.Find(id);
+
+            if (fileToRetrieve.PhotoBytes != null && fileToRetrieve.PhotoType != null)
+                return File(fileToRetrieve.PhotoBytes, fileToRetrieve.PhotoType);
+
+            return View();
+        }
+    }
+}
