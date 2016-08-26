b720d6294fd935afff2cf0e246e11342eb92f591
Prince <princemwilliams@gmail.com>
2016-08-26 11:11:59 -0400

    Merge wip

--- UFX-WCCI/Controllers/HomeController.cs
+++ UFX-WCCI/Controllers/HomeController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UFX_WCCI.Models;
 
 namespace UFX_WCCI.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
-        {
-            return View();
+        {  
+            return View(db.Postings.ToList());
         }
 
         public ActionResult About()
