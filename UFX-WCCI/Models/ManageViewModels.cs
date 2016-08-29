e6e0571f07080ae516b2c9782018760ab8b0f8d2
Joan Mayer Campbell <joanmayercampbell@gmail.com>
2016-08-29 16:47:13 -0400

    changing profile page

--- UFX-WCCI/Models/ManageViewModels.cs
+++ UFX-WCCI/Models/ManageViewModels.cs
@@ -8,6 +8,7 @@ namespace UFX_WCCI.Models
     public class IndexViewModel
     {
         public bool HasPassword { get; set; }
+        public string UserName { get; set; }
         public IList<UserLoginInfo> Logins { get; set; }
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
